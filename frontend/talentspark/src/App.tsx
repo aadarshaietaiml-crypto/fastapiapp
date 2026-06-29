d57c35fb81f30607f75ac63b2b4a3c09bf8a0cee
Shetty Aadarsh Ravi <aadarshaiet.aiml@gmail.com>
2026-06-29 16:47:56 +0530

    test git

--- frontend/talentspark/src/App.tsx
+++ frontend/talentspark/src/App.tsx
@@ -2,13 +2,16 @@ import Welcome from "./components/welcome";
 import NavBar from "./components/NavBar";
 import CompanyCard from "./components/CompanyCard";
 import JobCard from "./components/JobCard";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
     <NavBar />
     <Welcome />
+    <br/>
     <CompanyCard />
     <JobCard />
+    <Footer />
     </>
   );
 }
