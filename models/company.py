86010410ec1ad1497d04746da3d3e2a01600c66b
Shetty Aadarsh Ravi <aadarshaiet.aiml@gmail.com>
2026-06-29 10:27:17 +0530

    test git

--- models/company.py
+++ models/company.py
@@ -8,6 +8,6 @@ class Company(Base):
     name=Column(String,nullable=False,index=True)
     email=Column(String,unique=True)
     phone=Column(String,unique=True)
-    jobs=relationship("job",back_populates="company")
+    jobs=relationship("Job",back_populates="company")
 
 
